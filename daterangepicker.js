b77835c4f73a0790547945e3cefb0508e84ca0e0
adg <alessandro@simplaex.com>
2019-11-26 16:45:50 +0100

    RVR-3014 - add variables declarations

--- daterangepicker.js
+++ daterangepicker.js
@@ -605,7 +605,7 @@
             }
 
             var list = '<ul>';
-            for (range in this.ranges) {
+            for (let range in this.ranges) {
                 list += '<li data-range-key="' + range + '">' + range + '</li>';
             }
 
